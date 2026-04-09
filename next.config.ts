e85c112133e17c75c07684204d934757598f218e
Menace2societe <143929601+Menace2societe@users.noreply.github.com>
2026-04-09 06:07:12 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // Ceci force Vercel à ignorer les erreurs de type pour finir le build
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Ceci ignore les erreurs de style/linting pendant le build
     ignoreDuringBuilds: true,
   },
 };
+
+export default nextConfig;
